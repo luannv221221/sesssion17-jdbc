df0c25623ce6c65ea096818b0b7f11b25c1c9e9e
your new username <smallmonster900@gmail.com>
2024-09-11 21:42:16 +0700

    ok

--- src/business/CategoryBusiness.java
+++ src/business/CategoryBusiness.java
@@ -36,6 +36,37 @@ public class CategoryBusiness {
 
         return categories;
     }
+
+    // tìm về bản ghi theo id
+    public static Category findById(int id){
+        Category category = null;
+        Connection connection = null;
+        try {
+            connection = ConnectionDataBase.openConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM category WHERE id = ?");
+            statement.setInt(1,id);
+            ResultSet resultSet = statement.executeQuery();
+            category = new Category();
+            int count = 0;
+            while (resultSet.next()){
+                count++;
+                category.setId(resultSet.getInt("id"));
+                category.setName(resultSet.getString("name"));
+                category.setStatus(resultSet.getBoolean("status"));
+            }
+            System.out.println(count);
+            if(count == 0){
+                return null;
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            ConnectionDataBase.closeConnection(connection);
+        }
+
+        return category;
+    }
     // Thêm mới
     public static boolean create(Category category){
         Connection connection = null;
@@ -62,6 +93,52 @@ public class CategoryBusiness {
     }
 
     // Sửa
+    public static boolean update(Category category){
+        Connection connection = null;
 
+        try {
+            // tạo đối tượng kết nối
+            connection = ConnectionDataBase.openConnection();
+            // Tạo đối tượng PreparedStatement
+            String sql = "UPDATE category SET name = ?, status=? WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            // set các giá trị của tham số trong sql của prepareStatement
+            statement.setString(1,category.getName());
+            statement.setBoolean(2,category.isStatus());
+            statement.setInt(3,category.getId());
+            //thực thi câu truy vấn
+            statement.executeUpdate();
+            return true;
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            ConnectionDataBase.closeConnection(connection);
+        }
+
+        return false;
+    }
     // Xóa
+    public static boolean delete(int id){
+        Connection connection = null;
+        try {
+            // tạo đối tượng kết nối
+            connection = ConnectionDataBase.openConnection();
+            // Tạo đối tượng PreparedStatement
+            String sql = "DELETE FROM category WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            // set các giá trị của tham số trong sql của prepareStatement
+            statement.setInt(1,id);
+            //thực thi câu truy vấn
+            statement.executeUpdate();
+            return true;
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            ConnectionDataBase.closeConnection(connection);
+        }
+
+        return false;
+    }
+
+
 }
--- src/run/Program.java
+++ src/run/Program.java
@@ -26,8 +26,10 @@ public class Program {
                     Program.createCategory(scanner);
                     break;
                 case 3:
+                    Program.updateCategory(scanner);
                     break;
                 case 4:
+                    Program.deleteCategory(scanner);
                     break;
                 case 5:
                     System.exit(0);
@@ -53,6 +55,69 @@ public class Program {
             System.out.println("Thêm mới thành công");
         } else {
             System.err.println("Thêm mới thất bại");
+        }
+    }
+    // xây dựng phương thức cập nhật danh mục
+    public static void updateCategory(Scanner scanner){
+        // Bước 1. Nhập Id Cần sửa
+        System.out.println("Nhap vao id can sua ");
+        int idEdit = Integer.parseInt(scanner.nextLine());
+        // Hiển thị danh mục tìm được
+        Category category = CategoryBusiness.findById(idEdit);
+        // show danh mục
+        if(category != null){
+            System.out.println("Thông tin danh mục ");
+            category.showData();
+            // hỏi người dùng muốn update trường nào và nhập thông tin
+            boolean isExit = true;
+            do {
+                System.out.println("Chọn trường muốn cập nhật");
+                System.out.println("1. Sửa tên danh mục");
+                System.out.println("2. Sửa trạng thái");
+                System.out.println("3. Thoát ");
+                int choice = Integer.parseInt(scanner.nextLine());
+                switch (choice){
+                    case 1:
+                        System.out.println("Nhập vào tên mới");
+                        category.setName(scanner.nextLine());
+                        break;
+                    case 2:
+                        System.out.println("Nhập vào trạng thái");
+                        category.setStatus(Boolean.parseBoolean(scanner.nextLine()));
+                        break;
+                    case 3:
+                        isExit = false;
+                        break;
+                    default:
+                        System.out.println("Sai lua chọn");
+                }
+            }while (isExit);
+            // thực cập nhật trong database gọi đến update của CategoryBusiness
+            boolean result = CategoryBusiness.update(category);
+            if(result){
+                System.out.println("Cập nhật thanh công");
+            } else {
+                System.err.println("Cập nhật thất bại ");
+            }
+        } else {
+            System.out.println("Koo tim thay danh mục");
+        }
+
+    }
+
+    // phương thức xóa danh mục
+    public static void deleteCategory(Scanner scanner){
+        System.out.println("Mời bạn nhập vào id cần xóa ?");
+        int id = Integer.parseInt(scanner.nextLine());
+        if(CategoryBusiness.findById(id) != null){
+            boolean result = CategoryBusiness.delete(id);
+            if(result){
+                System.out.println("Xóa thành công ");
+            } else {
+                System.err.println("Xóa thất bại");
+            }
+        } else {
+            System.err.println("Mã danh mục không tồn tại");
         }
     }
 }
